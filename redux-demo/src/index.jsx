5b125c24b8a35fcb1c09d8b6afdec1e4ce112dde
justfantasy <27506954@qq.com>
2022-01-12 08:32:37 +0800

    使用react-redux的情况下不需要额外写监测redux值变化的代码

--- redux-demo/src/index.jsx
+++ redux-demo/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import store from './redux/store';
 import App from './App'; // 此处引入的组件可修改
 
 ReactDom.render(
@@ -9,12 +8,3 @@ ReactDom.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-store.subscribe(() => {
-  ReactDom.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
-});
