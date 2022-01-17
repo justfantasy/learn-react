28d8941397f5df628f419859e8f8527318f14d5c
justfantasy <27506954@qq.com>
2022-01-17 09:06:07 +0800

    联系hooks

--- hooks-demo/src/containers/Count/index.jsx
+++ hooks-demo/src/containers/Count/index.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactDom from 'react-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { increment, decrement } from '../../redux/actions/count';
 
 function Count(props) {
   const { count, increment: incr, decrement: decr } = props;
-  const ref = React.createRef();
-  const [person, setPerson] = React.useState({ id: '01', name: '小明' });
+  const ref = useRef(); // 使用createRef测试也是正常的。
+  const [person, setPerson] = useState({ id: '01', name: '小明' });
+
+  useEffect(() => {
+    // 默认执行componentDidMount
+    document.title = `You clicked ${count} times`;
+    const timer = setInterval(() => {
+      incr(1);
+    }, 1000);
+
+    // 返回回调函数，类似执行componentWillUnmount方法
+    return () => {
+      clearInterval(timer);
+    };
+  }, []); // 传入空数组，表示不执行componentDidUpdate方法，需要监测updated状态值传入指定的状态值 [count]
 
   const doIncr = () => {
     incr(+ref.current.value);
@@ -24,7 +38,11 @@ function Count(props) {
 
   const doChangePerson = () => {
     setPerson({ id: '02', name: '刘德华' });
-    // setPerson((obj) => ({ ...obj, name: '刘德华' }));
+    // setPerson((obj) => ({ ...obj, ...{ name: '刘德华' } }));
+  };
+
+  const doUnmount = () => {
+    ReactDom.unmountComponentAtNode(document.getElementById('root'));
   };
 
   return (
@@ -48,6 +66,8 @@ function Count(props) {
       <button type="button" onClick={doOddIncr}>当前求和为奇数加</button>
       &nbsp;
       <button type="button" onClick={doChangePerson}>更改用户</button>
+      &nbsp;
+      <button type="button" onClick={doUnmount}>卸载组件</button>
     </div>
   );
 }
