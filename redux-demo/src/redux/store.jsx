1487c97a891d51c80b10d54c5d7eb882605b328c
justfantasy <27506954@qq.com>
2022-04-21 22:19:47 +0800

    增加redux-thunk注释

--- redux-demo/src/redux/store.jsx
+++ redux-demo/src/redux/store.jsx
@@ -2,4 +2,5 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+// redux-thunk主要的功能就是可以让我们dispatch一个函数，而不只是普通的 Object
 export default createStore(reducers, applyMiddleware(thunk));
